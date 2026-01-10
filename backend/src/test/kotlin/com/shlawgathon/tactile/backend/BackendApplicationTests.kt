30b03782606abab9039ebd0befece6952b857f91
Subham K. <me@growly.gg>
2026-01-10 10:05:10 -0800

    feat: initial backend

---
+++ backend/src/test/kotlin/com/shlawgathon/tactile/backend/BackendApplicationTests.kt
@@ -0,0 +1,15 @@
+package com.shlawgathon.tactile.backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BackendApplicationTests
+{
+
+    @Test
+    fun contextLoads()
+    {
+    }
+
+}
